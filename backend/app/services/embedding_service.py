11a60140b67ad7aaf06d8541e9f54e46c3d5673d
Parvathy A <141492631+ParvathyArunachalam@users.noreply.github.com>
2026-07-18 11:09:18 +0000

    memory remove

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -1,12 +1,24 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    return model
 
 
 def create_embeddings(chunks):
     if not chunks:
         return []
 
-    embeddings = model.encode(chunks, convert_to_numpy=True)
+    embeddings = get_model().encode(
+        chunks,
+        convert_to_numpy=True
+    )
 
     return embeddings
